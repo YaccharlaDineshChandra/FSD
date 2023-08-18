02432ef0108676f74e2c9339df325ef6c6520319
YaccharlaDineshChandra <dineshchandradine013@gmail.com>
2023-08-18 11:22:11 +0530

    FSD

---
+++ Java Script/basics/external.js
@@ -0,0 +1,16 @@
+var a=1000;
+var d='pentapati';
+var b=200;
+var c=300;
+// displaying the output we use document.write()
+document.write("welcome to java Script </br>");
+document.write("value of a:",a);
+document.write("<br>"+typeof(a))
+// it displays the default value
+var k;
+document.write(k);
+document.write("</br>",d);
+// console.log() that displays the value in the inspect console
+console.log("this is c value=",c);
+// alert that display the value in a dialog box
+alert("this is b value="+b);
